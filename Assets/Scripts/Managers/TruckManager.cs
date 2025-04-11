982abcb67ece2a2a06254ed6de158784e8d8f577
Mireene-Unity <dhalsghkdxld@gmail.com>
2025-04-11 17:17:17 +0900

    [Feat]TruckManager

--- Assets/Scripts/Managers/TruckManager.cs
+++ Assets/Scripts/Managers/TruckManager.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class TruckManager
 {
+    [SerializeField] float _maxHP;
+    float _currHP;
     public void Init()
     {
-
+        _currHP = _maxHP;
+    }
+    public void GetDamage(int _damage)
+    {
+        _currHP -= _damage;
     }
 }
