8168231b74cdb0460ccf77ea97fbb3218a50d0e3
goalgoloo1 <goalgoloo1@gmail.com>
2025-04-11 15:24:41 +0900

    add MapNode

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -1,9 +1,29 @@
 using UnityEngine;
 
-public class MapManager
+
+//HS: TEMP Mono, I'll remove this later
+public class MapManager : MonoBehaviour  
 {
+    [SerializeField] private GameObject canvasGameObject; 
+
     public void Init()
     {
+    }
 
+    void Update()
+    {
+        //HS: TEMP Click, I'll remove this later and use action handler
+
+        if (Input.GetMouseButtonDown(0)) // 0 is for left mouse button
+        {
+            if (canvasGameObject != null)
+            {
+                canvasGameObject.SetActive(false); // Deactivate the Canvas GameObject
+            }
+            else
+            {
+                Debug.LogWarning("Canvas GameObject is not assigned in the MapManager.");
+            }
+        }
     }
 }
