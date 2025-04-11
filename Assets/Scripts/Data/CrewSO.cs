941a4c4a57f832ef79ca92f5e1fe02f1b94cfd32
sayishuu <smile52673@gmail.com>
2025-04-11 11:35:47 +0900

    [Feat] CrewSO 구현

--- Assets/Scripts/Data/CrewSO.cs
+++ Assets/Scripts/Data/CrewSO.cs
@@ -3,5 +3,34 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CrewSO", menuName = "Scriptable Objects/CrewSO")]
 public class CrewSO : ScriptableObject
 {
-    
+    [SerializeField] private int _crewId;
+
+    [Header("유닛 설정")]
+    [SerializeField] private string _name;
+    [SerializeField] private CrewRank _rank;
+    [SerializeField] private string _description;
+
+    [Header("초기 스탯")]
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private float _health;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _repairSpeed;
+    [SerializeField] private float _attackSpeed;
+    [SerializeField] private float _healSpeed;
+    [SerializeField] private float _avoidance;  //회피율
+
+    [Range(0,100)]
+    [SerializeField] private float _evilRate;   //성향
+
+    public int CrewId => _crewId;
+    public string Name => _name;
+    public CrewRank Rank => _rank;
+    public float MaxHealth => _maxHealth;
+    public float Health => _health;
+    public float MoveSpeed => _moveSpeed;
+    public float RepairSpeed => _repairSpeed;
+    public float AttackSpeed => _attackSpeed;
+    public float HealSpeed => _healSpeed;
+    public float Avoidance => _avoidance;
+    public float EvilRate => _evilRate;
 }
--- Assets/Scripts/Util/Define.cs
+++ Assets/Scripts/Util/Define.cs
@@ -5,6 +5,13 @@ public enum Example
     // 이런식으로 이넘타입을 전역으로 정의해서 이 클래스에서만 관리해주세요
 }
 
+public enum CrewRank
+{
+    A,
+    B,
+    C
+}
+
 public class Define
 {
     
