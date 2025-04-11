b6130d691c6c7d16e5249b09539c2ca1c222bb8d
goalgoloo1 <goalgoloo1@gmail.com>
2025-04-11 20:14:45 +0900

    [Feat] MapManager.cs

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -1,29 +1,24 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 
-//HS: TEMP Mono, I'll remove this later
-public class MapManager : MonoBehaviour  
+public class MapManager : MonoBehaviour
 {
-    [SerializeField] private GameObject canvasGameObject; 
-
+    [SerializeField]GameObject _canvas;
     public void Init()
     {
     }
 
-    void Update()
+    public void ExecuteGameEvents()
     {
-        //HS: TEMP Click, I'll remove this later and use action handler
-
-        if (Input.GetMouseButtonDown(0)) // 0 is for left mouse button
-        {
-            if (canvasGameObject != null)
-            {
-                canvasGameObject.SetActive(false); // Deactivate the Canvas GameObject
-            }
-            else
-            {
-                Debug.LogWarning("Canvas GameObject is not assigned in the MapManager.");
-            }
-        }
+        string[] events = { "전투이벤트 실행!", "수리이벤트 실행", "연료이벤트 실행!" };
+        int randomIndex = Random.Range(0, events.Length);
+        Debug.Log(events[randomIndex]);
+        _canvas.SetActive(false);
+        
     }
+
+
+
+
 }
