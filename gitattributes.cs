0f236b0df45a97fcdf6987a22373afedf3490171
Mireene-Unity <dhalsghkdxld@gmail.com>
2025-04-11 16:31:43 +0900

    [Feat] .gitattributes

---
+++ gitattributes.cs
@@ -0,0 +1,9 @@
+# C# 파일 UTF-8 인코딩 강제
+*.cs text working-tree-encoding=UTF-8
+
+# 텍스트 파일
+*.txt text working-tree-encoding=UTF-8
+
+# JSON, XML 등 추가 가능
+*.json text working-tree-encoding=UTF-8
+*.xml  text working-tree-encoding=UTF-8
